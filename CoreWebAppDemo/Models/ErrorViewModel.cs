e50201e08816744f35c53556cfe2b125bcdd78dd
unknown <sreedhara.bondugula>
2021-05-14 19:26:08 +0530

    first commit

---
+++ CoreWebAppDemo/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CoreWebAppDemo.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
